f0152a3a9d3a1b79b0e2bdb73a5fa98c402f0517
srinadh-code <yalagandulasrinadh@gmail.com>
2026-02-20 13:41:21 +0530

    t

--- details/settings.py
+++ details/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'signuplogin',
     'rest_framework',
+    'category',
 ]
 
 MIDDLEWARE = [
@@ -117,10 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-import os
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join( BASE_DIR,'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
